7c4c35dd4e24d92d493b98ffae021f452d09e3ec
chomba <chomba@bongohive.co.zm>
2022-05-03 19:56:40 +0200

    Initial Commit

---
+++ expenseTracker/expenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTrackerApp.swift
+//  expenseTracker
+//
+//  Created by Chomba Chanda on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct expenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
