9e8819c15cc0b75ff1d5e8c20a87a4876e858a0a
chomba <chomba@bongohive.co.zm>
2022-05-03 20:29:09 +0200

    Setup oveview page

---
+++ expenseTracker/Extentions.swift
@@ -0,0 +1,16 @@
+//
+//  Extentions.swift
+//  expenseTracker
+//
+//  Created by Chomba Chanda on 5/3/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
